ffe2735889fd26746ad205df69c604a98f94dec8
Ragdata <ragdata@users.noreply.github.com>
2025-02-17 10:21:51 +1000

    stabilise

--- src/stacks/core/stack.sh
+++ src/stacks/core/stack.sh
@@ -6,15 +6,15 @@ if [[ -z "$SCRIPT_DIR" ]]; then
     export SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 fi
 
-[[ ! -d "$DOCKER_VOL/traefik/cert" ]] && mkdir -p "$DOCKER_VOL/traefik/cert"
-[[ ! -d "$DOCKER_VOL/traefik/conf" ]] && mkdir -p "$DOCKER_VOL/traefik/conf"
-[[ ! -d "$DOCKER_VOL/traefik/logs" ]] && mkdir -p "$DOCKER_VOL/traefik/logs"
+[[ ! -d "/mnt/vol/docker/traefik/cert" ]] && mkdir -p /mnt/vol/docker/traefik/cert
+[[ ! -d "/mnt/vol/docker/traefik/conf" ]] && mkdir -p /mnt/vol/docker/traefik/conf
+[[ ! -d "/mnt/vol/docker/traefik/logs" ]] && mkdir -p /mnt/vol/docker/traefik/logs
 
 DATADIR="$(dirname $(dirname "$SCRIPT_DIR") )/data/traefik/volumes"
 
-cp "$DATADIR"/cert/* "$DOCKER_VOL"/traefik/cert/.
-cp "$DATADIR"/conf/* "$DOCKER_VOL"/traefik/conf/.
-cp "$DATADIR"/logs/* "$DOCKER_VOL"/traefik/logs/.
+cp ../../data/traefik/volumes/cert/* /mnt/vol/docker/traefik/cert/.
+cp ../../data/traefik/volumes/conf/* /mnt/vol/docker/traefik/conf/.
+cp ../../data/traefik/volumes/logs/* /mnt/vol/docker/traefik/logs/.
 
 
 
